bb5f20bd2ad2c22c03dbe3677bf3a5ff2a3e14c0
RomanT <roman.tara@holydaystudios.com>
2026-08-17 02:24:28 +0700

    UI for Dino Queue and Allocation added (base, no scripts)

---
+++ Assets/Project/Scripts/UI/DinoAllocationUI.cs
@@ -0,0 +1,16 @@
+using Project.Scripts.Utils;
+
+public class DinoAllocationUI : ContentUi
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
